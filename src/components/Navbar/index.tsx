4b9bd9ac5878077d581ab15484b35ac7d6dc660d
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-08-28 19:30:18 -0300

    Adding base colors and config to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,29 @@
-import { NavbarContainer } from "./styles";
+import { CartContainer, CityContainer, NavbarContainer, NavbarLocation } from "./styles";
 
 import Logo from "../../assets/images/logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
 
 export default function Navbar() {
   return (
     <NavbarContainer>
       <div className="logo">
         <img src={Logo} alt="logo" />
+      </div>
+      <div>
+        <NavbarLocation>
+          <CityContainer>
+            <MapPin
+              weight="fill"
+              size={32}
+              color="#8047F8"
+            />
+            <p>Belo Horizonte, MG</p>
+          </CityContainer>
+          <CartContainer>
+            <ShoppingCart size={32} color="#c47f17" />
+          </CartContainer>
+        </NavbarLocation>
+
       </div>
     </NavbarContainer>
   )
