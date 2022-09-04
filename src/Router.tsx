82c0af97830611ed113a535e565784d297ae76fd
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-09-04 13:04:21 -0300

    Adding finished route

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/finished" element={<Finished />} />
       </Route>
     </Routes>
   )
